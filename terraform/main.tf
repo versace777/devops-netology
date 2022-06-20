59b9bdd77e2b499f6201a36c84e567bc85c0f9c4
versace777 <47698474+versace777@users.noreply.github.com>
2022-06-20 15:36:54 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,86 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version =  ">= 0.13"
+}
 
+provider yandex {
+  token     = " "
+  cloud_id  = "ajete2md1hdc4eor977s"
+  folder_id = "b1gkig5sqsgm8q570bep"
+  zone      = "ru-central1-b"
+}
+
+resource yandex_compute_image ubu-img {
+  name          = "ubuntu-20-04-lts-v20210908"
+  source_image  = "fd81hgrcv6lsnkremf32"
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+locals {
+  instance = {
+  stage = 1
+  prod = 2
+  }
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "vm1"
+
+  resources {
+    cores  = "1"
+    memory = "2"
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "${yandex_compute_image.ubu-img.id}"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  count = local.instance[terraform.workspace]
+}
+
+locals {
+  id = toset([
+    "1",
+    "2",
+  ])
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "vm2"
+
+  resources {
+    cores  = "1"
+    memory = "2"
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "${yandex_compute_image.ubu-img.id}"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
